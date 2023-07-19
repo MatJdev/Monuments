9f13447d57fc0d654c99f5c0a8511faad99ce3d3
mjarquez <matias.jarquez@alten.es>
2023-07-19 10:30:34 +0200

    Fix Geocoder

--- commonFeatures/src/main/java/com/example/practica5/commonfeatures/util/NewMonumentUtils.kt
+++ commonFeatures/src/main/java/com/example/practica5/commonfeatures/util/NewMonumentUtils.kt
@@ -3,15 +3,20 @@ package com.example.practica5.commonfeatures.util
 import android.content.Context
 import android.location.Address
 import android.location.Geocoder
+import java.io.IOException
 import java.util.Locale
 
 object NewMonumentUtils {
     fun getCountryCodeFromLocation(context: Context, latitude: Double, longitude: Double): String? {
         val geocoder = Geocoder(context, Locale.getDefault())
-        val addresses: List<Address>? = geocoder.getFromLocation(latitude, longitude, 1)
+        try {
+            val addresses: List<Address>? = geocoder.getFromLocation(latitude, longitude, 1)
 
-        if (!addresses.isNullOrEmpty()) {
-            return addresses[0].countryCode
+            if (!addresses.isNullOrEmpty()) {
+                return addresses[0].countryCode
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
 
         return null
